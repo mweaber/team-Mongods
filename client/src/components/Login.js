7eb2cff1274b1e60eda377847a8d889fc786f660
Matthew Weaber <matthewweaber89@gmail.com>
2019-04-20 10:44:31 -0400

    Saved and Shop made, routes working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import Header from "./Header";
+
 
 class Login extends Component {
-render() {
-    return(
-
+    render() {
+        return(
             <div>
                 <h5>Here is some example text of where a user will come to login/signup</h5>
                 <p>This shit better work.</p>
@@ -12,16 +11,10 @@ render() {
             {/* <form action="Get Money">
                 Please choose a username:
                 <input type = "text">Username input</input>
-            </form> */}
-
-            
+            </form> */}           
             </div>
-
-
-    )
-}
-
-
+        );
+    }
 }
 
 
---
+++ client/src/components/Shop.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+class Shop extends Component {
+    render() {
+        return(
+            <div>
+                <h5>Here will be a api call to eBay to search for comics to buy</h5>
+                <p>I would also like to add in a yelp/google maps type section to find local shops.</p> 
+ 
+            </div>
+        );
+    }
+}
+
+
+
+export default Shop;
