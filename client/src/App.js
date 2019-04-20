d598201ed095956c0a516dd64a444a4d8ddd56b3
Matthew Weaber <matthewweaber89@gmail.com>
2019-04-20 09:28:54 -0400

    Deleted old comp folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App";
 import ComicCard from "./components/ComicCard";
 import Card from "./components/Card";
