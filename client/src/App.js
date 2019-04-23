0a991453ca2f88c5ca9ae09aa5c097149423d652
Matthew Weaber <matthewweaber89@gmail.com>
2019-04-23 18:53:25 -0400

    Router, Switch, Routes

--- client/src/App.js
+++ client/src/App.js
@@ -22,51 +22,19 @@ class App extends Component {
   }
   render() {
     return (
+      <Router>
+        <Header/>
         <div>
-        <div>
-
-          <Header />
-          <div className="row">
-            <Login />
-          </div>
-          <div className="row">
-            <div className="col s8 m8 l8">
-            <div className="container">
-              <ComicCard />
-            </div>
-            </div>
-            <div className="col s4 m4 l4">
-            
-              {
-                this.state.cards.map(card => (
-                <Card
-                  key={card.id}
-                  id={card.id}
-                  alt={card.name}
-                  image={card.image}
-                />
-              ))
-            }
-
-            </div>
-
-            <div className="body">
-              <h2>Body and things to work on:</h2>
-              <ul>
-                <li>Get comic API</li>
-                <li>Get the eBay API</li>
-                <li>Set up routes</li>
-                <li>Make a fav search</li>
-                <li>Maybe some games or little time wasters</li>
-              </ul>
-            </div>
-
-
-          </div>
-          <Footer />
-
+          <Switch>
+            <Route exact path= "/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/shop" component={Shop} />
+            <Route component = {ErrorPage} />
+          </Switch>
         </div>
-        </div>
+
+      </Router>
     );
   }
 }
