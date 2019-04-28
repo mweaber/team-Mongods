a4d1895179f2b894f0ed8b4d71e99ff9d4d4ecda
BBadoo <bbader0806@gmail.com>
2019-04-28 16:19:28 -0400

    working ish

--- routes/api/eBay.js
+++ routes/api/eBay.js
@@ -7,3 +7,22 @@ router
     .get(eBayController.findItemsByKeywords);
 
 module.exports = router;
+
+
+
+
+
+// Matches with the /saved
+router.get("/", (req, res) => {
+    res.json({
+        ok: "Hello sir thank you!",
+        tyler1: "what even is a <div> bro?",
+        tyler2: "Show me the code bro",
+        tyler3: ""
+        
+    })
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,9 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 
+const router = require("./routes")
+app.use(router)
+
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
