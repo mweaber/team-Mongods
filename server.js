8c09d7ee4f3b6978abee1838b6920b5b27f60108
Matthew Weaber <matthewweaber89@gmail.com>
2019-04-29 19:38:55 -0400

    Working on routes and DB

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const router = require("./routes");
+// const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,12 +14,8 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(router);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
