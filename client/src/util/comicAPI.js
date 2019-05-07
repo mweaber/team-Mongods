c12aea651f02cb452e5dc955124cb73714a80e8c
Matthew Weaber <matthewweaber89@gmail.com>
2019-05-07 15:02:14 -0400

    Updated index api file for routes

--- client/src/util/comicAPI.js
+++ client/src/util/comicAPI.js
@@ -3,12 +3,12 @@ import axios from "axios";
 export default {
     findCharacter: function(data) {
         console.log("Inside API file " + " " + data);
-    //    return axios.get("/character/search/" + data);
-    axios.get("https://comicvine.gamespot.com/api/characters/?api_key=c9fea9053bbb565b7f4b69a097b2f30be2fc3a56&format=json&filter=wolverine")
-    .then(function (response) {
-            console.log(response.data.results[0].aliases)
-            response.json(response.data)
-        })
-    .catch(err => data.json(err))
+       return axios.get("/character/search/" + data);
+//     axios.get("https://comicvine.gamespot.com/api/characters/?api_key=c9fea9053bbb565b7f4b69a097b2f30be2fc3a56&format=json&filter=wolverine")
+//     .then(function (response) {
+//             console.log(response.data.results[0].aliases)
+//             response.json(response.data)
+//         })
+//     .catch(err => data.json(err))
     }
 };
--- routes/api/comic.js
+++ routes/api/comic.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const axios = require("axios");
 
 
-router.get("/character/search", function (req, res) {
+router.get("/comic", function (req, res) {
     console.log("path hit in api file")
     axios.get("https://comicvine.gamespot.com/api/characters/?api_key=c9fea9053bbb565b7f4b69a097b2f30be2fc3a56&format=json&filter=" + (req.body.params))
     .then(function (response) {
